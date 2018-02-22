bb32fa31709f2ca45ec3d39f233b85ac322cde60
alexandroid1 <alex123456>
2018-02-22 13:47:52 +0200

    calcFeetAndInchesToCentimeters

--- src/com/globallogic/MethodOverloading.java
+++ src/com/globallogic/MethodOverloading.java
@@ -23,4 +23,15 @@ public class MethodOverloading {
         return 0;
     }
 
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println( feet + " feet, " + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
 }
