73016a1bc6012d4a688fc12023b9ff1d6d9ca247
alexandroid1 <alex123456>
2018-02-22 14:08:17 +0200

    calcFeetAndInchesToCentimeters 1

--- src/com/globallogic/MethodOverloading.java
+++ src/com/globallogic/MethodOverloading.java
@@ -26,6 +26,7 @@ public class MethodOverloading {
 
     public static double calcFeetAndInchesToCentimeters(double feet, double inches){
         if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
             return -1;
         }
 
